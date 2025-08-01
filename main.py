3e3dedbd459f59fd75efff8a4f247fb9790fa967
Infernw <tomi02fasasi@gmail.com>
2025-08-01 18:01:51 +0100

    printing report and adding new books

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
+import sys
+
 from stats import get_num_words
 from stats import get_characters_counts
+from stats import sorted_char_counts
 
 def get_book_text(filepath):
     with open(filepath) as f:
         content = f.read()
         return content
     
+def print_report(num_words, count_list, filepath):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath}")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for i in count_list:
+        print(f"{i['char']}: {i['num']}")
+    
 def main():
-    text = get_book_text("books/frankenstein.txt")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    filepath = sys.argv[1]
+    text = get_book_text(filepath)
     num_words = get_num_words(text)
     characters_counts = get_characters_counts(text)
-    print(f"{num_words} words found in the document")
-    print(characters_counts)
+    count_list = sorted_char_counts(characters_counts)
+    print_report(num_words, count_list, filepath)
 
 main()
--- stats.py
+++ stats.py
@@ -7,9 +7,21 @@ def get_characters_counts(text):
     Dict = {}
     text = text.lower()
     for i in text:
-        if i in Dict.keys():
+        if i in Dict:
             Dict[i] += 1
         else:
             Dict[i] = 1
 
     return Dict
+
+def sort_on(items):
+    return items["num"]
+
+def sorted_char_counts(count_dict):
+    char_list = []
+    for i in count_dict:
+        if i.isalpha():
+            char_list.append({"char": i, "num": count_dict[i]})
+
+    char_list.sort(reverse=True, key=sort_on)
+    return char_list
