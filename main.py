cf4777b943a38c4d1548bc12427ba433e88b6945
Infernw <tomi02fasasi@gmail.com>
2025-08-10 23:54:00 +0100

    handled case of book not in file + user can now input the book's name instead of full filepath

--- main.py
+++ main.py
@@ -4,14 +4,19 @@ from stats import get_num_words
 from stats import get_characters_counts
 from stats import sorted_char_counts
 
-def get_book_text(filepath):
-    with open(filepath) as f:
-        content = f.read()
-        return content
+def get_book_text(book):
+    filepath = f"books/{book}.txt"
+    try:
+        with open(filepath) as f:
+            content = f.read()
+            return content
+    except FileNotFoundError:
+        print(f"{book} is not in the database")
+        sys.exit(1)
     
-def print_report(num_words, count_list, filepath):
+def print_report(num_words, count_list, book):
     print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {filepath}")
+    print(f"Analyzing {book}")
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
     print("--------- Character Count -------")
@@ -20,14 +25,19 @@ def print_report(num_words, count_list, filepath):
     
 def main():
     if len(sys.argv) != 2:
-        print("Usage: python3 main.py <path_to_book>")
+        print("Usage: python3 main.py <bookname>")
         sys.exit(1)
     
-    filepath = sys.argv[1]
-    text = get_book_text(filepath)
+    book_name = sys.argv[1].capitalize()
+    words = book_name.split()
+    book = "".join(words)
+    book = book.lower()
+    
+
+    text = get_book_text(book)
     num_words = get_num_words(text)
     characters_counts = get_characters_counts(text)
     count_list = sorted_char_counts(characters_counts)
-    print_report(num_words, count_list, filepath)
+    print_report(num_words, count_list, book_name)
 
 main()
